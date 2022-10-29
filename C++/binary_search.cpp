ad294efdeea67c5186978ec2f54021beea5eb571
manish singh tomar <72705246+manishsinghtomar@users.noreply.github.com>
2022-10-29 22:07:02 +0530

    binary_search created

---
+++ C++/binary_search.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std
+int binarysearch(arr[],int size,int key){
+	start=0;end=size-1;
+	arr[i]=start+(end-start)/2;
+	if(arr[i]==key){
+		return i;
+	if(arr)
+	}
+}
+
+
+
+int main()
+{
+	
+	return 0;
+	
